e2dd0504668ba826db786ed9ad0a20ae5458a4d5
Rodrigo Bandeira <rodrigodeimbassai@gmai.com>
2019-05-18 23:22:44 -0300

    issue with standard deviation

--- test-beam.py
+++ test-beam.py
@@ -1,25 +1,32 @@
 import apache_beam as beam
+import numpy as np
 from apache_beam.options.pipeline_options import PipelineOptions
+from sys import argv
+# Use Python argparse module to parse custom arguments
+import argparse
+
 
 # defining custom arguments
-class MyOptions(PipelineOptions):    
-  @classmethod
-  def _add_argparse_args(cls, parser):
-    parser.add_argument('--input',
-                        help='Input for the pipeline',
-                        default='./data/')
-    parser.add_argument('--output',
-                        help='Output for the pipeline',
-                        default='./output/')
+class MyOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument('--input',
+                            help='Input for the pipeline',
+                            default='./data/')
+        parser.add_argument('--output',
+                            help='Output for the pipeline',
+                            default='./output/')
+
 
 # class to split a csv line by elements and keep only the columns we are interested in 
 class Split(beam.DoFn):
     def process(self, element):
-        Date,Open,High,Low,Close,Volume = element.split(",")
+        Date, Open, High, Low, Close, Volume = element.split(",")
         return [{
             'Date': Date,
             'Open': float(Open),
-            'Close': float(Close)
+            'Close': float(Close),
+            'Volume': float(Volume)
         }]
 
 
@@ -29,14 +36,22 @@ class CollectOpen(beam.DoFn):
         result = [(1, element['Open'])]
         return result
 
+
 class CollectClose(beam.DoFn):
     def process(self, element):
         # Returns a list of tuples containing the 1 key and Close value
         result = [(1, element['Close'])]
         return result
 
+class CollectVolume(beam.DoFn):
+    def process(self, element):
+        # Returns a list of tuples containing the 1 key and Close value
+        result = [(1, element['Open'])]
+        return result
+
+
 # class to calculate the standard deviation over an entire PCollection
-class Standard_deviation(beam.DoFn):
+class Standard_deviation(beam.CombineFn):
     def create_accumulator(self):
         return (0.0, 0.0, 0) # x, x^2, count
 
@@ -68,12 +83,24 @@ class Standard_deviation(beam.DoFn):
                 'count': 0
             }
 
-# setting input and output files
-input_filename = "c:/tmp/sp500.csv"
-output_filename = "c:/tmp/output/result.txt"
 
 # instantiate the pipeline
 options = PipelineOptions()
+options = PipelineOptions(flags=argv)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input')
+parser.add_argument('--output')
+known_args, pipeline_args = parser.parse_known_args(argv)
+
+# setting input and output files
+input_filename = "./data/sp500.csv"
+output_filename = "./data/result.txt"
+
+if known_args.input:
+    input_filename = known_args.input
+if known_args.output:
+    output_filename = known_args.output
 
 with beam.Pipeline(options=options) as p:
     # reading the csv and splitting lines by elements we want to retain
@@ -100,12 +127,17 @@ with beam.Pipeline(options=options) as p:
             )
         )
 
+    std_dev = (
+        csv_lines | beam.ParDo(CollectVolume()) | beam.GroupByKey() | beam.CombineValues(Standard_deviation())
+        )
+
     # writing results to file
-    output= ( 
-        { 
+    output = (
+        {
             'Mean Open': mean_open,
-            'Mean Close': mean_close 
-        } | 
-        beam.CoGroupByKey() | 
+            'Mean Close': mean_close,
+            'Std Dev': std_dev
+        } |
+        beam.CoGroupByKey() |
         beam.io.WriteToText(output_filename)
     )
